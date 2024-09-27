64962f020dbd507891a5add4d55ea7832b545302
Terrana Willma <terrana.w@gmail.com>
2024-09-27 13:36:50 +0700

    import image logo & cart

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
+import Logo from '../assets/KEANE-log.png'
+import Cart from '../assets/carts.png'
 
 function Navbar({ totalItems }) {
 
   return (
     <nav className="bg-black border-b-2 p-1 flex justify-between items-center sticky top-0 ">
-      <img className="h-20" src="../src/assets/KEANE-Log.png" />
+      <img src={Logo} className="h-20" />
       <div className="text-white flex items-center">
-        <span className="size-10 "><img src="./src/assets/carts.png" alt="cart" /></span>
+        <span className="size-10 "><img src={Cart} alt="cart" /></span>
         <span className="-ml-2 mb-0  bg-red-500 rounded-full w-7 h-7 flex items-center justify-center text-white">
           {totalItems}
         </span>
